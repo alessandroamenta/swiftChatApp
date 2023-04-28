4ae066d526b8873bf0d0997bef08027a2d68c978
alessandroamenta <alessandroamenta1@gmail.com>
2023-04-28 14:06:18 -0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Alessandro Amenta on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
