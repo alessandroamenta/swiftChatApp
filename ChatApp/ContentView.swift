eb7e7334eb3942b30ba8cbbaff7ef2570d86247f
alessandroamenta <alessandroamenta1@gmail.com>
2023-04-28 18:18:19 -0700

    Initial commit

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var messagesManager = MessagesManager()    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                TitleRow()
+                
+                ScrollView {
+                    ForEach(messagesManager.messages, id: \.id){ message in
+                        MessageBubble(message: message)
+                    }
+                }
+                .padding(.top, 10)
+                .background(.white)
+                .cornerRadius(30, corners: [.topLeft, .topRight])
+            }
+            .background(Color("Peach"))
+            
+            MessageField()
+                .environmentObject(messagesManager)
         }
-        .padding()
     }
 }
 
